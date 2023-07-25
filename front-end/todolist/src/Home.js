851eb7b381566c06fdda6cd53ca994d376f8c335
mlnxe777 <melanie.dao@epitech.eu>
2023-07-25 14:32:51 +0200

    setup the front-end

---
+++ front-end/todolist/src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <React.Fragment>
+            <h1 className="text-3xl">
+                test
+            </h1>
+        </React.Fragment>
+    )
+}
+export default Home;
