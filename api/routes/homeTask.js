e80fec70695cb4f0602e4d6049e03fa5505ac43f
mlnxe777 <melanie.dao@epitech.eu>
2023-07-26 23:23:03 +0200

    add route to delete a task

--- api/routes/homeTask.js
+++ api/routes/homeTask.js
@@ -5,7 +5,7 @@ const taskCollection = require("../schema/taskSchema");
 router.get('/', async function (req, res, next) {
    const task = await taskCollection.find();
    res.send(task);
-});
+})
 
 router.post('/', async (req, res) => {
    const data = {
@@ -13,7 +13,14 @@ router.post('/', async (req, res) => {
    }
    const insert = await taskCollection.insertMany([data]);
    res.send(insert);
-},
-)
+})
+router.delete("/todo/:id", async (req, res, next) => {
+   try {
+      await taskCollection.findByIdAndRemove(req.params.id)
+      return success(res, "task deleted")
+   } catch (err) {
+      next({ status: 400, message: "failed to delete the task" })
+   }
+})
 
 module.exports = router;
